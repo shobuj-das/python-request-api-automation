b96a4f068980718dd187037a51adb65292a8d852
Shobuj Das <dasshobuj@gmail.com>
2026-07-10 23:18:01 +0600

    end points updated

--- end_points.py
+++ end_points.py
@@ -2,7 +2,7 @@ class EndPoint:
     BASE_URL = 'https://restful-booker.herokuapp.com'
 
     AUTH = f'{BASE_URL}/auth'
-    ALL_BOOKINGS = f'{BASE_URL}/bookings'
+    ALL_BOOKINGS = f'{BASE_URL}/booking'
     GET_BOOKING_BY_ID = f'{BASE_URL}/booking'
     CREATE_BOOKING = f'{BASE_URL}/booking'
     UPDATE_BOOKING = f'{BASE_URL}/booking'
