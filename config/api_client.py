f6e7d3bbf0ecd738bd5f5c239394e0fbd928e789
Shobuj Das <dasshobuj@gmail.com>
2026-07-14 00:52:24 +0600

    logging/added

--- config/api_client.py
+++ config/api_client.py
@@ -1,8 +1,12 @@
 import requests
 
 from config.settings import BASE_URL
+from utils.logger import get_logger
 
-class ApiClient: 
+logger = get_logger(__name__)
+
+
+class ApiClient:
 
     def request(
         self,
@@ -12,14 +16,31 @@ class ApiClient:
         payload=None,
         params=None
     ):
+
         url = BASE_URL + endpoint
 
-        response = requests.request(
-            method=method,
-            url=url,
-            headers=headers,
-            json=payload,
-            params=params
+        # Request Log
+        logger.info("=" * 80)
+        logger.info(f"REQUEST METHOD : {method}")
+        logger.info(f"URL            : {url}")
+        logger.info(f"HEADERS        : {headers}")
+        logger.info(f"PARAMS         : {params}")
+        logger.info(f"PAYLOAD        : {payload}")
+
+        try:
+            response = requests.request(
+                method=method,
+                url=url,
+                headers=headers,
+                json=payload,
+                params=params
         )
+        except Exception as e:
+            logger.exception(f"Request fail: {e}")
+
+        # Response Log
+        logger.info(f"STATUS CODE    : {response.status_code}")
+        logger.info(f"RESPONSE BODY  : {response.text}")
+        logger.info("=" * 80)
 
         return response
