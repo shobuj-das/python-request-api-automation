6fcbbff38a4968d4e861e6827248ac59a76f1529
Shobuj Das <dasshobuj@gmail.com>
2026-07-20 23:53:07 +0600

    fix/delete booking

--- tests/test_delete_booking.py
+++ tests/test_delete_booking.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 
 from config.end_points import EndPoint
 from config.http_methods import HttpMethod
@@ -7,13 +8,24 @@ from utils.booking_assertion_helper import BookingAssertion
 
 @pytest.mark.smoke
 def test_delete_booking(auth_token,client):
-    id = 9
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
         "Cookie": f"token={auth_token}"
     }
 
+    id = -1
+    while id == -1:
+        random_booking_id = random.randint(1,100)
+        response = client.request(
+            method=HttpMethod.GET,
+            endpoint=f"{EndPoint.GET_BOOKING_BY_ID}/{random_booking_id}",
+        )
+        if response.status_code == 200:
+            id = random_booking_id
+            break
+        
+    
     response = client.request(
         method=HttpMethod.DELETE,
         endpoint=f"{EndPoint.DELETE_BOOKING}/{id}",
